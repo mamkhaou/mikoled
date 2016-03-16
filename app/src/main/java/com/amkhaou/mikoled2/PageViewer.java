57a931ce5876027d2d67d12f4fb66dee858e4e5d
mohammed <mohammed.amkhaou@gmail.com>
2016-03-16 17:22:28 +0100

    add: *add actionbar to fragmentactivity pageviewer *show name of connected device on actionbar *add exit  without action till now *change background of first activity devicechooser *change size of devices list

--- app/src/main/java/com/amkhaou/mikoled2/PageViewer.java
+++ app/src/main/java/com/amkhaou/mikoled2/PageViewer.java
@@ -4,15 +4,18 @@ import android.app.ActionBar;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.Menu;
 import android.support.v7.widget.Toolbar;
 
-public class PageViewer extends FragmentActivity {
+public class PageViewer extends ActionBarActivity {
 
     ViewPager mainpager;
     protected String mMikoname = null;
+    protected android.support.v7.app.ActionBar mActionBar ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,15 @@ public class PageViewer extends FragmentActivity {
         Intent intent =  getIntent();
         mMikoname= intent.getStringExtra("MIKONAME");
 
+        //change actionbar name
+        mActionBar = getSupportActionBar();
+        mActionBar.setTitle(Html.fromHtml("<small>"+mMikoname+"</small>"));
+
+       /* getSupportActionBar().setLogo(R.drawable.light_bulb_4);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);*/
+
+
         // start service foreground service
         Intent serviceintent = new Intent(this,MikoService.class);
         serviceintent.putExtra("MIKONAME", mMikoname);
@@ -37,7 +49,7 @@ public class PageViewer extends FragmentActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_control,menu);
+        getMenuInflater().inflate(R.menu.menu_control, menu);
         return true;
     }
 }
